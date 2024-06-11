ba6d7d9ef6dad37bc6505aa753571e5f769d6e04
immanuellking <emmaebubeegbosi@gmail.com>
2024-06-11 01:02:09 +0100

    removed sync cus theres nore prmise function in the page

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -5,7 +5,7 @@ import { Suspense } from 'react';
 import { CardsSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
 import CardsWrapper from '../cards';
 
-export default async function Page() {
+export default function Page() {
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
