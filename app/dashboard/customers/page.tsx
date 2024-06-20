9905bb0d86a9f81be4dd4bc6efed78b77e16adf2
immanuellking <emmaebubeegbosi@gmail.com>
2024-06-20 18:45:05 +0100

    added customers table code and aslo fetched the code for the filtered customers

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,33 @@
-export default function Page() {
+import Pagination from '@/app/ui/invoices/pagination';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/invoices/table';
+import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import { fetchCustomers, fetchFilteredCustomers, fetchInvoicesPages } from '@/app/lib/data';
+import { Metadata } from 'next';
+import CustomersTable from '@/app/ui/customers/table';
+
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  // const currentPage = Number(searchParams?.page) || 1;
+
+  const customers = await fetchFilteredCustomers(query);
+
   return (
-    <div>Customers Page</div>
-  )
+    <div className="w-full">
+      <CustomersTable customers={customers} />
+    </div>
+  );
 }
