6fcac74414dd9445aa20e007aa5d0d9b0f696478
immanuellking <emmaebubeegbosi@gmail.com>
2024-06-20 16:55:27 +0100

    cahnged to a client component and use useFormState for dispathcing form action and data and also displaying error

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { lusitana } from '@/app/ui/fonts';
 import {
   AtSymbolIcon,
@@ -6,10 +7,19 @@ import {
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
+import { authenticate } from '../lib/action';
+import { useFormState } from 'react-dom';
 
 export default function LoginForm() {
+  const initialState = {
+    errorMessage: '',
+  };
+  const [state, dispatch] = useFormState(authenticate, initialState);
+
+  console.log(state);
+
   return (
-    <form className="space-y-3">
+    <form className="space-y-3" action={dispatch}>
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
           Please log in to continue.
@@ -56,8 +66,17 @@ export default function LoginForm() {
           </div>
         </div>
         <LoginButton />
-        <div className="flex h-8 items-end space-x-1">
-          {/* Add form errors here */}
+        <div
+          className="flex h-8 items-end space-x-1"
+          aria-live="polite"
+          aria-atomic="true"
+        >
+          {state?.errorMessage && (
+            <>
+              <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
+              <p className="text-sm text-red-500">{state.errorMessage}</p>
+            </>
+          )}
         </div>
       </div>
     </form>
